25e4acb20b38ee4573d75361cf15aff5651591e7
Munzir Rosdi <munzir@gmail.com>
2020-02-11 21:13:31 +0800

    initial

---
+++ src/pages/Index.vue
@@ -0,0 +1,205 @@
+<template>
+  <Layout>
+    
+    <div class="flex flex-wrap -mx-2">
+      <div class="w-full lg:w-4/5 px-2">
+        <h2 class="text-3xl font-semibold mb-2">Lenses</h2>
+        <div class="items-container">
+          <div class="item">
+            <div class="border-b border-gray-100 p-3">
+              <h3 class="font-semibold text-gray-500 text-sm uppercase">Nikon</h3>
+            </div>
+            <div class="p-3">
+              <ul>
+                <li class="mb-1"> 
+                  <a href="#" class>Nikon 28-300mm VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-300mm f/3.5-6.3G VR II <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-35mm <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Tamron 24-70mm G2 <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 16-80mm f/2.4-4E VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+             <div class="border-b border-gray-100 p-3">
+              <h3 class="font-semibold text-gray-500 text-sm uppercase">Canon</h3>
+            </div>
+            <div class="p-3">
+              <ul>
+                <li class="mb-1"> 
+                  <a href="#" class>Nikon 28-300mm VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-300mm f/3.5-6.3G VR II <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-35mm <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Tamron 24-70mm G2 <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 16-80mm f/2.4-4E VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+         <div class="item">
+             <div class="border-b border-gray-100 p-3">
+              <h3 class="font-semibold text-gray-500 text-sm uppercase">Sony</h3>
+            </div>
+            <div class="p-3">
+              <ul>
+                <li class="mb-1"> 
+                  <a href="#" class>Nikon 28-300mm VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-300mm f/3.5-6.3G VR II <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-35mm <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Tamron 24-70mm G2 <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 16-80mm f/2.4-4E VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+             <div class="border-b border-gray-100 p-3">
+              <h3 class="font-semibold text-gray-500 text-sm uppercase">Fujifilm</h3>
+            </div>
+            <div class="p-3">
+              <ul>
+                <li class="mb-1"> 
+                  <a href="#" class>Nikon 28-300mm VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-300mm f/3.5-6.3G VR II <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-35mm <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Tamron 24-70mm G2 <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 16-80mm f/2.4-4E VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+             <div class="border-b border-gray-100 p-3">
+              <h3 class="font-semibold text-gray-500 text-sm uppercase">Panasonic</h3>
+            </div>
+            <div class="p-3">
+              <ul>
+                <li class="mb-1"> 
+                  <a href="#" class>Leica DG 8-18mm F2.8-4.0 </a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Leica DG 12-60mm F2.8-4.0 </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+             <div class="border-b border-gray-100 p-3">
+              <h3 class="font-semibold text-gray-500 text-sm uppercase">Olympus</h3>
+            </div>
+            <div class="p-3">
+              <ul>
+                <li class="mb-1"> 
+                  <a href="#" class>Nikon 28-300mm VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-300mm f/3.5-6.3G VR II <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 18-35mm <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Tamron 24-70mm G2 <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">FX</span></a>
+                </li>
+                <li class="mb-1">
+                  <a href="#" class>Nikon 16-80mm f/2.4-4E VR <span class="text-center text-xs rounded-sm bg-indigo-400 text-white px-1">DX</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/5 px-2 text-sm">
+        <div class="p-3 rounded bg-gray-100">
+          
+        <h3 class="font-bold mb-2">Tags</h3>
+          <a href="#" class="tag-item">#Full-Frame</a>
+          <a href="#" class="tag-item">#APS-C</a>
+          <a href="#" class="tag-item">#Canon</a>
+          <a href="#" class="tag-item">#Nikon</a>
+          <a href="#" class="tag-item">#Sony</a>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Best list of recommended travel lenses & gears"
+  }
+};
+</script>
+
+<style>
+.items-container {
+  column-gap: 1.5em;
+  min-height:100vh;
+}
+
+.item {
+  margin: 0 0 1.5em;
+  width:100%;
+  display:inline-block;
+  @apply bg-white shadow-sm border;
+}
+
+.items-container a {
+  transition: color 0.15s;
+  @apply text-blue-500;
+}
+
+.items-container a:hover {
+  @apply text-blue-700;
+}
+
+.tag-item {
+  @apply text-indigo-700 py-1 px-1 rounded-full mb-2  block;
+}
+
+@media only screen and (min-width: 768px) {
+  .items-container {
+    column-count: 3;
+  }
+}
+
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .items-container {
+    column-count: 1;
+  }
+}
+</style>
